ab49531b66f30b14cc310355965ee68669dfd717
피수영 <vltndud123@naver.com>
2021-07-19 06:19:45 +0900

    C++ : 플로이드(11404) 문제 소스코드 업로드

---
+++ BOJ/플로이드(11404).cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int city[101][101];
+
+#define INF 987654321
+
+void floyed_washall() {
+    // i : mid, j : src, k : dst
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            for(int k = 1; k <= n; k++) {
+                city[j][k] = min(city[j][k], city[j][i] + city[i][k]);
+            }
+        }
+    }
+}
+
+void set_city_arr() {
+    for(int i = 0; i < 101; i++) {
+        for(int j = 0; j < 101; j++) {
+            city[i][j] = INF;
+        }
+    }
+}
+
+void print_city_arr() {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(city[i][j] == INF || i == j)
+                city[i][j] = 0;
+            cout << city[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
+
+int main(void) {
+    set_city_arr();
+    
+    cin >> n >> m;
+    
+    for(int i = 0; i < m; i++) {
+        int src, dst, cost;
+        
+        cin >> src >> dst >> cost;
+        
+        if(cost < city[src][dst])
+            city[src][dst] = cost;
+    }
+    
+    floyed_washall();
+    print_city_arr();
+}
