0c49e4f00127c60749435e76b848751d796182f1
피수영 <vltndud123@naver.com>
2021-07-23 18:16:05 +0900

    C++ : 스타트링크(5014) 문제 재채점 후, Out of bounds 에러 발생 문제 해결

--- BOJ/스타트링크(5014).cpp
+++ BOJ/스타트링크(5014).cpp
@@ -1,51 +1,51 @@
-//#include <iostream>
-//#include <queue>
-//
-//using namespace std;
-//
-//int F, S, G, U, D;
-//bool visit[1000001] = {false, };
-//
-//int BFS() {
-//    
-//    queue<pair<int, int>> q;
-//    
-//    q.push({S, 0});
-//    visit[S] = true;
-//    
-//    while(!q.empty()) {
-//        int curFloor = q.front().first;
-//        int curCnt = q.front().second;
-//        q.pop();
-//        
-//        if(curFloor == G) {
-//            return curCnt;
-//        }
-//        else {
-//            if(!visit[curFloor + U] && curFloor + U <= F) {
-//                q.push({curFloor + U, curCnt + 1});
-//                visit[curFloor + U] = true;
-//            }
-//            
-//            if(!visit[curFloor - D] && curFloor - D >= 1) {
-//                q.push({curFloor - D, curCnt + 1});
-//                visit[curFloor - D] = true;
-//            }
-//        }
-//    }
-//    
-//    return -1;
-//}
-//
-//int main(void) {
-//    
-//    cin >> F >> S >> G >> U >> D;
-//    
-//    int ans = BFS();
-//    
-//    if(ans == -1) {
-//        cout << "use the stairs\n";
-//    }
-//    else
-//        cout << ans << '\n';
-//}
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int F, S, G, U, D;
+bool visit[1000001] = {false, };
+
+int BFS() {
+    
+    queue<pair<int, int>> q;
+    
+    q.push({S, 0});
+    visit[S] = true;
+    
+    while(!q.empty()) {
+        int curFloor = q.front().first;
+        int curCnt = q.front().second;
+        q.pop();
+        
+        if(curFloor == G) {
+            return curCnt;
+        }
+        else {
+            if(curFloor + U <= F && !visit[curFloor + U]) {
+                q.push({curFloor + U, curCnt + 1});
+                visit[curFloor + U] = true;
+            }
+            
+            if(curFloor - D >= 1 && !visit[curFloor - D]) {
+                q.push({curFloor - D, curCnt + 1});
+                visit[curFloor - D] = true;
+            }
+        }
+    }
+    
+    return -1;
+}
+
+int main(void) {
+    
+    cin >> F >> S >> G >> U >> D;
+    
+    int ans = BFS();
+    
+    if(ans == -1) {
+        cout << "use the stairs\n";
+    }
+    else
+        cout << ans << '\n';
+}
