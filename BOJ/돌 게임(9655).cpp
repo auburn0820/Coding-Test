23d3bf227bbe057d7311cc9dfbe4c4c401406aaf
피수영 <vltndud123@naver.com>
2021-07-25 18:28:59 +0900

    C++ : 돌 게임(9655) 문제 소스코드 업로드

---
+++ BOJ/돌 게임(9655).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int N;
+    cin >> N;
+    
+    if(N % 2 == 1) {
+        cout << "SK\n";
+    } else {
+        cout << "CY\n";
+    }
+}
