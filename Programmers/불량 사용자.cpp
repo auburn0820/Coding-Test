5b8727e74c4c5799b03e7001d601c6462c12793f
피수영 <vltndud123@naver.com>
2021-09-07 10:37:02 +0900

    Programmers/C++ : 불량 사용자 문제 연결리스트 풀이 소스코드 수정 (비트 마스킹으로 키 값 사용)

--- Programmers/불량 사용자.cpp
+++ Programmers/불량 사용자.cpp
@@ -1,105 +1,62 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 #include <set>
-#include <cstring>
+#include <bitset>
+#include <iostream>
 
 using namespace std;
 
-set<string> for_unique;
-bool *is_visit;
+bool is_visit[10] = {false, };
+set<int> s;
 
-bool is_same_size(string &user_id, string &banned_id) {
-    if(user_id.size() == banned_id.size())
-        return true;
-    else
+bool is_banned_user(const string &user_id, const string &banned_id) {
+    if(user_id.size() != banned_id.size())
         return false;
-}
-
-bool is_same_type(string &user_id, string &banned_id) {
-    size_t length = user_id.size();
-    
-    for(int i = 0; i < length; i++) {
-        if(banned_id[i] == '*')
-            continue;
-        if(user_id[i] != banned_id[i])
+    for(size_t i = 0; i < user_id.size(); i++) {
+        if(user_id[i] != banned_id[i] && banned_id[i] != '*')
             return false;
     }
     return true;
 }
 
-bool is_banned_id(string &user_id, string &banned_id) {
-    if(!is_same_size(user_id, banned_id))
-        return false;
-    if(!is_same_type(user_id, banned_id))
-        return false;
-    
-    return true;
-}
-
-void comb(int cur, vector<string> &user_id, vector<string> &banned_id, vector<string> id, int &answer) {
-    // 경우의 수를 뽑은 경우
-    if(id.size() == banned_id.size()) {
-        int banned_id_cnt = 0;
+void combination(size_t idx, vector<string> &user_id, vector<string> &banned_id, int &answer) {
+    if(idx == banned_id.size()) {
         
-        vector<string> temp_id(id.begin(), id.end());
+        int key = 0;
         
-        // 제재 당한 아이디와 형식이 일치하는 아이디를 찾음
-        for(int i = 0; i < banned_id.size(); i++) {
-            for(int j = 0; j < temp_id.size();) {
-                if(is_banned_id(temp_id[j], banned_id[i])) {
-                    banned_id_cnt++;
-                    temp_id.erase(temp_id.begin() + j);
-                    break;
-                }
-                else
-                    j++;
+        for(size_t i = 0; i < user_id.size(); i++) {
+            if(is_visit[i]) {
+                key <<= 1;
+                key += 1;
+            }
+            else {
+                key <<= 1;
+                key += 0;
             }
         }
         
-        temp_id = vector<string>(id.begin(), id.end());
+        auto it = s.find(key);
         
-        // 제재당한 아이디가 일치하는 경우의 수를 뽑은 경우
-        if(banned_id_cnt == banned_id.size()) {
-            string key;
-            
-            sort(temp_id.begin(), temp_id.end());
-            
-            // 순열을 통해 뽑기 때문에 같은 경우가 있는지 확인
-            for(auto &i : temp_id)
-                key += i;
-            
-            auto it = for_unique.find(key);
-            
-            if(it == for_unique.end()) {
-                for_unique.insert(key);
-                answer++;
-            }
+        if(it == s.end()) {
+            answer++;
+            s.insert(key);
         }
+        
         return;
     }
     
-    for(int i = 0; i < (int)user_id.size(); i++) {
-        if(is_visit[i])
+    for(size_t i = 0; i < user_id.size(); i++) {
+        if(is_visit[i] || !is_banned_user(user_id[i], banned_id[idx]))
             continue;
         
         is_visit[i] = true;
-        id.push_back(user_id[i]);
-        comb(i + 1, user_id, banned_id, id, answer);
-        id.pop_back();
+        combination(idx + 1, user_id, banned_id, answer);
         is_visit[i] = false;
     }
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
     int answer = 0;
-    
-    vector<string> v;
-    is_visit = new bool[user_id.size()];
-    memset(is_visit, false, sizeof(bool) * user_id.size());
-    
-    comb(0, user_id, banned_id, v, answer);
-    
+    combination(0, user_id, banned_id, answer);
     return answer;
 }
